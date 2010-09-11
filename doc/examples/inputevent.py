5226b0088f2361c309e1d5a7753264e0c3ab485e
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-09-11 12:58:09 +0200

    Enhanced the inputevent example.

--- doc/examples/inputevent.py
+++ doc/examples/inputevent.py
@@ -1,6 +1,14 @@
 from twisted.internet import reactor
-from sparked.hardware.inputevent import InputEventDevice, InputEventProtocol
+from sparked.hardware.inputevent import InputEventDevice, InputEventProtocol, EV_KEY, EV_REL
 
-d = InputEventDevice(InputEventProtocol(), "/dev/input/by-id/usb-Griffin_Technology__Inc._Griffin_PowerMate-event-if00", reactor)
+class PowermateProtocol (InputEventProtocol):
+
+    def eventReceived(self, e):
+        if e.etype == EV_KEY:
+            print "Button", e.evalue
+        if e.etype == EV_REL:
+            print "Move", e.evalue
+
+d = InputEventDevice(PowermateProtocol(), "/dev/input/by-id/usb-Griffin_Technology__Inc._Griffin_PowerMate-event-if00", reactor)
 
 reactor.run()
