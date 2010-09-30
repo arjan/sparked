89fe4449dfd7140a365113fb2a52f4e0ad990fdf
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-09-30 15:20:24 +0200

    Updated serial, video examples

--- doc/examples/serial.py
+++ doc/examples/serial.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2010 Arjan Scherpenisse
+# See LICENSE for details.
+
+"""
+Demonistrating the IProtocolProbe functionality.
+"""
+
 import sys
 
 from zope.interface import implements
--- doc/examples/video.py
+++ doc/examples/video.py
@@ -1,3 +1,13 @@
+# Copyright (c) 2010 Arjan Scherpenisse
+# See LICENSE for details.
+
+"""
+An example that launches a fullscreen stage and puts live video
+streams on it for the webcams that you plug in. Hotpluggin is
+supported: the webcams will start streaming as soon as the device is
+detected.
+"""
+
 import clutter
 import cluttergst
 import gst
@@ -7,6 +17,12 @@ from sparked import application
 from sparked.hardware import video
 from sparked.graphics import stage
 
+class Options(application.Options):
+    optParameters = [
+        ["resolution", "r", "640x480", "Camera resolution. Use 'auto' for best resolution."]
+        ]
+
+__version__ = "0.1.0"
 
 
 class Application(application.Application):
@@ -19,24 +35,25 @@ class Application(application.Application):
         m.setServiceParent(self)
 
 
-
 class Stage(stage.Stage):
 
     def addDevice(self, dev):
         """
-        Gets a L{video.V4LDevice}, wraps it in a gst videosink and puts it on the clutter stage.
+        Gets a L{video.V4LDevice}, wraps it in a gst videosink and
+        puts it on the clutter stage on a random position.
         """
         tex = clutter.Texture()
         tex.show()
         self.add(tex)
         sink = cluttergst.VideoSink(tex)
-        
-        pipeline = gst.parse_launch(dev.getPipeline("video/x-raw-yuv", "320x240") + " ! queue name=t")
+
+        pipeline = gst.parse_launch(dev.getPipeline("video/x-raw-yuv", self.app.appOpts["resolution"]) + " ! queue name=t")
         pipeline.add(sink)
         gst.element_link_many(pipeline.get_by_name("t"), sink)
         pipeline.set_state(gst.STATE_PLAYING)
 
-        #stage.positionInBox(tex, self)
+        from random import random
+        tex.set_position(random()*(self.get_width()-tex.get_width()), random()*(self.get_height()-tex.get_height()))
 
 
 
@@ -44,16 +61,14 @@ class VideoMonitor(video.V4LDeviceMonitor):
     """
     Watches for cameras. If a camera connects, its live feed is started and put on the stage.
     """
-    
+
     def __init__(self, app):
         video.V4LDeviceMonitor.__init__(self)
         self.app = app
 
     def deviceAdded(self, i):
         dev = video.V4LDevice(i['unique_path'], i['version'])
-        print dev
         self.app.stage.addDevice(dev)
-
 
     def deviceRemoved(self, i):
         pass
