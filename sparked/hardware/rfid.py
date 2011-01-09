b6b638e7a96ca1617ea75179691593668628d615
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-01-09 10:53:42 +0100

    Added SonMicro RFID reader.

--- sparked/hardware/rfid.py
+++ sparked/hardware/rfid.py
@@ -9,6 +9,7 @@ RFID readers which work over serial.
 from zope.interface import implements, Interface, Attribute
 
 from twisted.internet import task
+from twisted.python import log
 
 from sparked.hardware.serialcommand import SerialCommandProtocol
 from sparked.hardware.serialport import IProtocolProbe
@@ -47,7 +48,7 @@ class IRFIDReaderProtocol(Interface):
 
 
 class SL031Protocol (SerialCommandProtocol):
-    implements (IProtocolProbe)
+    implements (IProtocolProbe, IRFIDReaderProtocol)
 
     # IProtocolProbe variables
     probeRequest = "\xBA\x02\x01\xB9"
@@ -83,6 +84,55 @@ class SL031Protocol (SerialCommandProtocol):
 
 
 
+class SonMicroProtocol (SerialCommandProtocol):
+    implements (IProtocolProbe, IRFIDReaderProtocol)
+
+    probeRequest = "\xFF\x00\x01\x83\x84"
+    probeResponse = "\xFF\x00\x02\x83\x4E\xD3"
+
+    sndPreamble = "\xFF\x00"
+    rcvPreamble = "\xFF\x00"
+    lengthIncludesChecksum = False
+
+    commands = [ ("SELECT", 0x83),
+                 ("FIRMWARE", 0x81) ]
+
+    pollInterval = 0.2
+
+    logTraffic = False
+
+    typemap = {0x01: TagType.MIFARE_ULTRALIGHT,
+               0x02: TagType.MIFARE_1K,
+               0x03: TagType.MIFARE_4K,
+               0xFF: TagType.UNKNOWN}
+
+
+    def got_FIRMWARE(self, data):
+        log.msg("%s - Firmware: %s" % (repr(self), data))
+
+
+    def got_SELECT(self, data):
+        if data == "N":
+            # no tag present
+            return
+        if data == "U":
+            # RF field off
+            return
+        tagtype = self.typemap[ord(data[0])]
+        tag = "".join("%02X" % ord(c) for c in data[1:])
+        self.events.dispatch("tag-present", tagtype, tag)
+
+
+    def start(self):
+        self._poller = task.LoopingCall(self.sendCommand, "SELECT")
+        self._poller.start(self.pollInterval)
+
+
+    def stop(self):
+        self._poller.stop()
+
+
+
 class RFIDReader (object):
 
     timeout = 0.5
@@ -90,14 +140,16 @@ class RFIDReader (object):
     protocol = None
 
     def __init__(self, protocol, reactor=None):
-        assert IRFIDReaderProtocol.implementedBy(protocol)
+        assert IRFIDReaderProtocol.implementedBy(protocol.__class__)
 
-        self.events = events.eventDispatcher()
+        self.events = EventDispatcher()
         self.events.setEventParent(events)
 
         self.protocol = protocol
-        self.protocol.events.addListener("tag-present", self.gotTag)
+        self.protocol.events.addObserver("tag-present", self.gotTag)
         self.protocol.events.setEventParent(self.events)
+
+        self.protocol.start()
 
         self.tags = {}
         if reactor is None:
